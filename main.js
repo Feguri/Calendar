834bab2245e37cda89534e3e1f2f83fda5a80636
Felipe Serrou Camy de Paiva <felipeserrou@hotmail.com>
2021-12-06 16:47:11 -0500

    added front end select days week but tasknum bug

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const date = new Date();
+let dataBase = {
+  'adderShown': false,
+}
+
+// starting on monday
+let daysOfTheWeekSelected = [false, false, false, false, false, false, false];
 
 const colorOfTheMonths = {
     "January": '#673ab7',
@@ -29,11 +35,42 @@ function extractNum(str) {
     return Number(filteredStr);
 }
 
+function firstThreeLetters(str){
+  let final = '';
+  for (let i = 0; i < 3; i++){
+    if (i === 0){
+      final += str[i].toUpperCase();
+    } else {
+      final += str[i];
+    }
+  }
+  return final;
+}
+
+for (let dayOfTheWeek of document.getElementsByClassName('circle-week')){
+  dayOfTheWeek.addEventListener('click', function(){
+
+    if(daysOfTheWeekSelected[extractNum(this.id)]){
+
+      dayOfTheWeek.style.backgroundColor = '#222227';
+      dayOfTheWeek.style.color = 'white';
+      daysOfTheWeekSelected[extractNum(this.id)] = false;
+
+    } else {
+
+      dayOfTheWeek.style.backgroundColor = 'white';
+      dayOfTheWeek.style.color = 'black';
+      daysOfTheWeekSelected[extractNum(this.id)] = true;
+      
+    }
+
+  });
+}
 const renderCalendar = () => {
   date.setDate(1);
 
   const monthDays = document.querySelector(".days");
-
+ 
   const lastDay = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
@@ -104,9 +141,13 @@ const renderCalendar = () => {
   for(let num of toIterate){
 
     function changeDay(){
-      
+
       // insert code here. also, load up current day based on local storage
-      console.log(this.id);
+      // oh and save the color of the task in local storage. it's nice.
+      let month = firstThreeLetters(document.getElementById('month').innerHTML);
+      let day = extractNum(this.id);
+      document.getElementById('date-date').innerHTML = `${month} ${day}`;
+      console.log(month, day);
     };
 
     document.getElementById(`calendar-day-${num}`).addEventListener('click', changeDay);
@@ -161,7 +202,13 @@ function addTask(){
     document.getElementById(`time-${taskNum}`).innerHTML = time1;
     document.getElementById(`time-2-${taskNum}`).innerHTML = time2;
 
-    dataBase[`day-${taskNum}`] = [taskInput, time1, time2, taskNum];
+    let currColor = 0;
+
+    let color = `background-color: ${colors[currColor]}`;
+    console.log(color);
+    
+
+    dataBase[`day-${taskNum}`] = [taskInput, time1, time2, taskNum, color];
 
 
     function Delete(){
@@ -169,17 +216,23 @@ function addTask(){
         delete dataBase[`day-${extractNum(this.id)}`];
     }
     
-    let currColor = 0;
+
     function changeColor(e){
 
         document.getElementById(`time-${e.target.here}`).style.backgroundColor = colors[currColor];
         document.getElementById(`time-2-${e.target.here}`).style.backgroundColor = colors[currColor];
         document.getElementById(`tt-${e.target.here}`).style.borderLeft = `3px solid ${colors[currColor]}`;
 
+        color = `background-color: ${colors[currColor]}`;
+
+        dataBase[`day-${e.target.here}`][4] = color; 
+        
         currColor++;
         if (currColor === 12){
             currColor = 0;
         }
+
+        console.log(color);
     }
 
     for (let el of document.getElementsByClassName('delete')){
@@ -215,10 +268,6 @@ function showAdder(){
 }
 document.getElementById('adder').addEventListener('click', showAdder);
 
-let dataBase = {
-    'adderShown': false,
-}
-
 window.onbeforeunload = function(){
     localStorage.setItem('database', JSON.stringify(dataBase));
     localStorage.setItem('taskNum', JSON.stringify(taskNum));
@@ -231,17 +280,16 @@ window.onload =  function(){
 
 
     let savedDataBase = JSON.parse(localStorage.getItem('database'));
-    let savedTaskNum = JSON.parse(localStorage.getItem('taskNum'))
+    let savedTaskNum = JSON.parse(localStorage.getItem('taskNum'));
+    
     if (dataBase !== null){
       dataBase = savedDataBase;
       taskNum = savedTaskNum;  
     }
+    // taskNum bug FIX IT
 
-    
     // em vez de tasknum use info pega na localStorage
     for (let key in dataBase){
-
-        console.log(dataBase[key][0]);
 
         if (dataBase[key][0] !== undefined){
 
@@ -249,19 +297,20 @@ window.onload =  function(){
             let savedTime1 = dataBase[key][1];
             let savedTime2 = dataBase[key][2];
             let savedTaskNum = dataBase[key][3];
+            let savedColor = dataBase[key][4];
 
             document.querySelector('.actual-tasks').innerHTML += `
             <div class="task-${savedTaskNum} mb-6" id="task-${savedTaskNum}">
                 <button class="delete is-large" id="delete-${savedTaskNum}"></button>
                 <div class="mb-5">
-                    <span class="time is-size-5" id="time-${savedTaskNum}">${savedTime1}</span>
+                    <span class="time is-size-5" id="time-${savedTaskNum}" style="${savedColor}">${savedTime1}</span>
                 </div>
                 <div class="text-task" id="tt-${savedTaskNum}">
                     <p id="${savedTaskNum}">${savedTextContent}</p>
         
                 </div>
                 <div class="bottom-time">
-                    <span class="time is-size-5" id="time-2-${savedTaskNum}">${savedTime2}</span>
+                    <span class="time is-size-5" id="time-2-${savedTaskNum}" style="${savedColor}">${savedTime2}</span>
                 </div>
             </div>`;
         }
